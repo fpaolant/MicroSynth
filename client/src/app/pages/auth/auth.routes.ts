d809662a16b52b8091af244bad319a5929624f9b
fpaolantonio <fabio.paolantonio@telespazio.com>
2025-06-26 09:14:48 +0000

    aggiunta auth guard a rotta change.password

--- client/src/app/pages/auth/auth.routes.ts
+++ client/src/app/pages/auth/auth.routes.ts
@@ -4,11 +4,12 @@ import { Login } from './login';
 import { Error } from './error';
 import { Signup } from './signup';
 import { ChangePassword } from './change.password';
+import { authGuard } from '../../services/auth.guard';
 
 export default [
     { path: 'access', component: Access },
     { path: 'error', component: Error },
     { path: 'login', component: Login },
     { path: 'signup', component: Signup},
-    { path: 'change-password', component: ChangePassword}
+    { path: 'change-password', component: ChangePassword, canActivate:  [authGuard]}
 ] as Routes;
